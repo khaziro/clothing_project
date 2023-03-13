a74cff39d2f9fdcfdbab8c3fb4516f8f125f15f5
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-13 11:07:48 +0200

    Created the navigation component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+  return (
+    <div className="shop">I am the shopping page</div>
+  )
+}
 const App = () => {
   return (
     <Routes>
-      <Route
-        path='/'
-        element={<Home/>}/>
+      <Route path='/'
+             element={<Navigation/>}>
+        <Route
+          index
+          element={<Home/>}/>
+        <Route
+          path='shop'
+          element={<Shop/>}
+        />
+      </Route>
+
     </Routes>)
 }
 
